18f04edca15aea86c4e6e29dac70cf63b8eadccd
Jeremiah <Breni.jeremiah.2021@gmail.com>
2024-07-31 02:32:12 -0700

    Add JS Doc and Refactor to new changes

--- js/dijkstra.js
+++ js/dijkstra.js
@@ -1,11 +1,15 @@
-import { coord, highlightCell, clearGrid, blueCellPosition, redCellPosition } from './grid.js';
+import { highlightCell, clearGrid, blueCellPosition, redCellPosition } from './grid.js';
 import { PriorityQueue } from './p_queue.js';
 import { Statistics } from './statistics.js';
+import { getNeighbors, reconstructPath } from './pathUtils.js';
 
-const matrix = coord;
 let running = false;
 export let stats = new Statistics('Dijkstra');
 
+/**
+ * Runs the Dijkstra algorithm to find the shortest path.
+ * @returns {Promise<void>}
+ */
 export async function dijkstra() {
     stats.reset();
     clearGrid();
@@ -13,24 +17,26 @@ export async function dijkstra() {
     running = true;
     const start = {x: blueCellPosition.x , y: blueCellPosition.y};
     const goal = {x: redCellPosition.x , y: redCellPosition.y};
-    const frontier = new PriorityQueue();
-    const evaluated = new Set();
-    const cost = new Map();
-    const totalCost = new Map();
-    const cameFrom = new Map();
+
+    const frontier = new PriorityQueue(); // keep track of the cells to visit
+    const evaluated = new Set(); // keep track of the cells that have been visited
+    const cost = new Map(); // keep track of the cost to reach a cell
+
+    const cameFrom = new Map(); // keep track of the optimal path
+
 
     cost.set(start, 0);
     frontier.enqueue(start, 0);
     stats.startTimer();
     while (!frontier.isEmpty() && running) {
         stats.step();
-        document.getElementById('steps').innerText = steps;
 
         const current = frontier.dequeue();
 
+        // Reached Goal
         if (current.x === goal.x && current.y === goal.y) {
             stats.stopTimer();
-            stats.updateStats();
+            stats.update();
             return reconstructPath(cameFrom, current);
         }
 
@@ -38,18 +44,23 @@ export async function dijkstra() {
 
         const neighbors = getNeighbors(current);
         for (let neighbor of neighbors) {
-            if (evaluated.has(neighbor)) {
+            stats.step();
+            if (evaluated.has(neighbor)) { // check if the cell has been visited already
                 continue;
             }
 
-            const tentativecost = cost.get(current);
-            if (!cost.has(neighbor) || tentativecost < cost.get(neighbor)) {
+            let weight = neighbor.weight;
+            const potentialCost = cost.get(current) + weight;
+
+            if (!cost.has(neighbor) || potentialCost < cost.get(neighbor)) { // check if the new path is better
                 cameFrom.set(neighbor, current);
-                cost.set(neighbor, tentativecost);
-                if (!frontier.elements.some(e => e.element === neighbor)) {
-                    stats.visit();
-                    frontier.enqueue(neighbor, totalCost.get(neighbor));
+                cost.set(neighbor, potentialCost);
+
+                if (!frontier.elements.some(e => e.element === neighbor)) { // check if the cell is already in the frontier
+                    frontier.enqueue(neighbor, potentialCost); // Use potentialCost as priority
+
                     highlightCell(neighbor.x, neighbor.y, '#FFD700');
+                    stats.visit();
                 }
             }
 
@@ -60,73 +71,22 @@ export async function dijkstra() {
         if (!document.getElementById('sleep').checked) await sleep(10); // Pause to visualize
         }
     }
-
-    return []; // No path found
 }
 
-function getNeighbors(current) {
-    const neighbors = [];
-    let directions = [];
-    let diagonal = document.getElementById('diagonalMovementCheckbox').checked;
-    if (diagonal) {
-        directions = [
-            { dx: -1, dy: 0 }, // left
-            { dx: 1, dy: 0 },  // right
-            { dx: 0, dy: -1 }, // up
-            { dx: 0, dy: 1 },  // down
-            { dx: -1, dy: -1 }, // top-left
-            { dx: 1, dy: -1 },  // top-right
-            { dx: -1, dy: 1 },  // bottom-left
-            { dx: 1, dy: 1 }    // bottom-right
-        ];
-    } else {
-        directions = [
-            { dx: -1, dy: 0 }, // left
-            { dx: 1, dy: 0 },  // right
-            { dx: 0, dy: -1 }, // up
-            { dx: 0, dy: 1 }  // down
-        ];
-    }
-
-
-    for (let { dx, dy } of directions) {
-        const newX = current.x + dx;
-        const newY = current.y + dy;
-        if (isValidCell(newX, newY)) {
-            highlightCell(newX, newY, '#87CEEB');
-            neighbors.push(matrix[newY][newX]);
-        }
-    }
-
-    return neighbors;
-}
-
-function isValidCell(x, y) {
-    return x >= 0 && x < matrix.length && y >= 0 && y < matrix[0].length && matrix[y][x].color !== 'black';
-}
-
-function reconstructPath(cameFrom, current) {
-    const path = [];
-    while (current) {
-        path.push(current);
-        current = cameFrom.get(current);
-
-    }
-    for (let cell of path.reverse()) {
-        stats.path();
-        document.getElementById('path-length').innerText = pathLength;
-        highlightCell(cell.x, cell.y, 'green');
-    };
-    stats.update();
-}
-
+/**
+ * Pauses execution for a given number of milliseconds.
+ * @param {number} ms - The number of milliseconds to sleep.
+ * @returns {Promise<void>}
+ */
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+/**
+ * Stops the Dijkstra algorithm.
+ */
 export function stop() {
     running = false;
-    const endTime = Date.now();
-    const totalTimeElapsed = endTime - startTime;
-    updateStats(steps, totalTimeElapsed, pathLength);
+    stats.stopTimer();
+    stats.update();
 }
