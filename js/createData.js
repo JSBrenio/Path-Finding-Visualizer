bfd0a239f5224ed8637a8e539d1ebfe17df44d17
Jsbrenio <brenio.jeremiah.2021@gmail.com>
2024-08-03 01:33:26 -0700

    Add sleep between data collection iterations to ensure images are saved

--- js/createData.js
+++ js/createData.js
@@ -3,7 +3,7 @@ import { aStar, stats as aStats} from './a_star.js';
 import { dijkstra, stats as dijStats } from './dijkstra.js';
 import { saveImage } from './main.js';
 
-export function collectData(trials = document.getElementById('trials').value) {
+export async function collectData(trials = document.getElementById('trials').value) {
     console.clear();
     document.getElementById('diagonalMovementCheckbox').checked = true;
     document.getElementById('sleep').checked = true;
@@ -21,11 +21,15 @@ export function collectData(trials = document.getElementById('trials').value) {
         saveImage(i);
         manhattan.push({'steps': aStats.steps, 'ms': aStats.time, 'length': aStats.pathLength, 'nodes': aStats.visitedNodes});
 
+        await sleep(1000);
+
         document.getElementById('heuristicDropdown').value = 'euclidean';
         aStar();
         aStats.update();
         saveImage(i);
         euclidean.push({'steps': aStats.steps, 'ms': aStats.time, 'length': aStats.pathLength, 'nodes': aStats.visitedNodes});
+
+        await sleep(1000);
 
         document.getElementById('heuristicDropdown').value = 'chebyshev';
         aStar();
@@ -33,17 +37,27 @@ export function collectData(trials = document.getElementById('trials').value) {
         saveImage(i);
         chebyshev.push({'steps': aStats.steps, 'ms': aStats.time, 'length': aStats.pathLength, 'nodes': aStats.visitedNodes});
 
+        await sleep(1000);
+
         document.getElementById('heuristicDropdown').value = 'octile';
         aStar();
         aStats.update();
         saveImage(i);
         octile.push({'steps': aStats.steps, 'ms': aStats.time, 'length': aStats.pathLength, 'nodes': aStats.visitedNodes});
 
+        await sleep(1000);
+
         document.getElementById('dijkstraCheckbox').click();
         dijkstra();
         dijStats.update();
         saveImage(i);
         dijkstraStats.push({'steps': dijStats.steps, 'ms': dijStats.time, 'length': dijStats.pathLength, 'nodes': dijStats.visitedNodes});
+
+        await sleep(1000); // Sleep for 1 second
+        // Function to sleep for a given number of milliseconds
+        function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
     }
 
     console.log("Manhattan Heuristic:");
--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ function selectAlgorithm(algorithm) {
     document.getElementById('bfsCheckbox').checked = (algorithm === 'bfs');
     document.getElementById('aStarCheckbox').checked = (algorithm === 'aStar');
     document.getElementById('dijkstraCheckbox').checked = (algorithm === 'dijkstra');
-
+    
     // Show or hide the heuristic dropdown
     const heuristicSelection = document.getElementById('heuristic-selection');
     if (algorithm === 'aStar') {
@@ -74,62 +74,68 @@ function save() {
 }
 
 export function saveImage(number = 0) {
-    const gridCanvas = document.getElementById('gridCanvas');
-    const stats = document.getElementById('algorithm-stats');
-    const overlay = document.getElementById('overlay');
-
-    // Create a new canvas to combine the grid and statistics and overlay
-    const combinedCanvas = document.createElement('canvas');
-    const ctx = combinedCanvas.getContext('2d');
-
-    // Set the new canvas size to fit both the grid and the statistics
-    combinedCanvas.width = gridCanvas.width;
-    combinedCanvas.height = gridCanvas.height + 200; // Adjust height to fit one line of text
-
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, combinedCanvas.width, combinedCanvas.height);
-    // Draw the grid
-    ctx.drawImage(gridCanvas, 0, 0);
-
-    // Draw the overlay (debug coordinates)
-    const overlayElements = overlay.children;
-    for (let i = 0; i < overlayElements.length; i++) {
-        const element = overlayElements[i];
-        const x = parseInt(element.style.left, 10);
-        const y = parseInt(element.style.top, 10);
-        ctx.font = '12px Arial';
-        ctx.fillStyle = 'black';
-        // Split the text into lines and draw each line separately
-        const lines = element.innerText.split('\n');
-        for (let j = 0; j < lines.length; j++) {
-            ctx.fillText(lines[j], x, y + 10 + (j * 14)); // Adjust y-coordinate for each line
+    return new Promise((resolve, _reject) => {
+        const gridCanvas = document.getElementById('gridCanvas');
+        const stats = document.getElementById('algorithm-stats');
+        const overlay = document.getElementById('overlay');
+        
+        // Create a new canvas to combine the grid and statistics and overlay
+        const combinedCanvas = document.createElement('canvas');
+        const ctx = combinedCanvas.getContext('2d');
+        
+        // Set the new canvas size to fit both the grid and the statistics
+        combinedCanvas.width = gridCanvas.width;
+        combinedCanvas.height = gridCanvas.height + 200; // Adjust height to fit one line of text
+        
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, combinedCanvas.width, combinedCanvas.height);
+        // Draw the grid
+        ctx.drawImage(gridCanvas, 0, 0);
+        
+        // Draw the overlay (debug coordinates)
+        const overlayElements = overlay.children;
+        for (let i = 0; i < overlayElements.length; i++) {
+            const element = overlayElements[i];
+            const x = parseInt(element.style.left, 10);
+            const y = parseInt(element.style.top, 10);
+            ctx.font = '12px Arial';
+            ctx.fillStyle = 'black';
+            // Split the text into lines and draw each line separately
+            const lines = element.innerText.split('\n');
+            for (let j = 0; j < lines.length; j++) {
+                ctx.fillText(lines[j], x, y + 10 + (j * 14)); // Adjust y-coordinate for each line
+            }
         }
-    }
+        
+        const statsLines = stats.innerText.split('\n');
+        const statsText = statsLines.slice(2, -2).join(' '); // Skip the first line and join the rest
+        
+        // Draw the statistics
+        ctx.font = '28px Arial';
+        ctx.fillStyle = 'black';
+        ctx.fillText(statsText, 10, gridCanvas.height + 30);
+        
+        if (selectedAlgorithm === 'aStar') {
+            ctx.fillText('Algorithm: A*', 10, gridCanvas.height + 60);
+            const heuristic = document.getElementById('heuristicDropdown').value;
+            ctx.fillText(` Heuristic: ${heuristic}`, 175, gridCanvas.height + 60);
+        } else {
+            ctx.fillText(`Algorithm: ${selectedAlgorithm}`, 10, gridCanvas.height + 60);
+        }
+        
+        ctx.fillText(`Diagonal Movement: ${document.getElementById('diagonalMovementCheckbox').checked ? 'Enabled' : 'Disabled'}`, 10, gridCanvas.height + 90);
+        ctx.fillText(`Weighted: ${document.getElementById('weight').checked ? 'Enabled' : 'Disabled'}`, 10, gridCanvas.height + 120);
+        // Save as an image
+        const link = document.createElement('a');
+        link.href = combinedCanvas.toDataURL('image/png');
+        link.download = `${number + '_' + 
+        (selectedAlgorithm === 'aStar' ? selectedAlgorithm + '_' + document.getElementById('heuristicDropdown').value : selectedAlgorithm)}.png`;
+        document.body.appendChild(link); // Append to body to ensure it can be clicked
+        link.click();
+        document.body.removeChild(link); // Clean up
 
-    const statsLines = stats.innerText.split('\n');
-    const statsText = statsLines.slice(2, -2).join(' '); // Skip the first line and join the rest
-
-    // Draw the statistics
-    ctx.font = '28px Arial';
-    ctx.fillStyle = 'black';
-    ctx.fillText(statsText, 10, gridCanvas.height + 30);
-    
-    if (selectedAlgorithm === 'aStar') {
-        ctx.fillText('Algorithm: A*', 10, gridCanvas.height + 60);
-        const heuristic = document.getElementById('heuristicDropdown').value;
-        ctx.fillText(` Heuristic: ${heuristic}`, 175, gridCanvas.height + 60);
-    } else {
-        ctx.fillText(`Algorithm: ${selectedAlgorithm}`, 10, gridCanvas.height + 60);
-    }
-
-    ctx.fillText(`Diagonal Movement: ${document.getElementById('diagonalMovementCheckbox').checked ? 'Enabled' : 'Disabled'}`, 10, gridCanvas.height + 90);
-    ctx.fillText(`Weighted: ${document.getElementById('weight').checked ? 'Enabled' : 'Disabled'}`, 10, gridCanvas.height + 120);
-    // Save as an image
-    const link = document.createElement('a');
-    link.href = combinedCanvas.toDataURL('image/png');
-    link.download = `${number + '_' + 
-    (selectedAlgorithm === 'aStar' ? selectedAlgorithm + '_' + document.getElementById('heuristicDropdown').value : selectedAlgorithm)}.png`;
-    link.click();
+        setTimeout(resolve, 5000); // Delay to allow the download to complete
+    });
 }
 
 init();
